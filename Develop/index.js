6f4c4b23e3120af9e2a5d5e93b6acaf7c9e98768
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-02-18 02:01:04 -0600

    get and post routes

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const { notes } = require('./db/db');
 
+function addNewNote(body, noteArray) {
+    const note = body;
+    noteArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: noteArray }, null, 2)
+    );
+    return note;
+}
 
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // add note to json file and notes array in this function
+    const note = addNewNote(req.body, notes);
+
+    res.json(note);
 });
 
 app.listen(PORT, () => {
